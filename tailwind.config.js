7e06be5885bd386ab779e261df24caa819a6420d
hammadrehman786 <hammadrehmanali007@gmail.com>
2026-06-24 18:01:03 +0200

    Add Vite config, Tailwind setup, and React component structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        burger: '#D4573F',
+        gold: '#FFA500',
+        dark: '#1a1a1a',
+      }
+    },
+  },
+  plugins: [],
+}
